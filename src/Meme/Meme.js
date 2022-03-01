c6ef1185fa99e37b860d0edb4a38af6c2b5a1774
jcsgh <business.joshuac@gmail.com>
2022-03-01 13:46:16 -0600

    added shuffle function and generate button

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -3,22 +3,34 @@ import styles from './styles.module.css'
  
 export const Meme = () => {
 
-    const[memes, setMemes] = useState([]);
+    const [memes, setMemes] = useState([]);
+    const [memeIndex, setMemeIndex] = useState(0);
+    
+    const shuffleMemes = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * i);
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+      };
 
 
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes').then(res=>
         res.json().then(res=>{
-            const memes = res.data.memes;
-            setMemes(memes);
+            const _memes = res.data.memes;
+            shuffleMemes(_memes);
+            setMemes(_memes);
         }))
     }, [])
 
     return(
         memes.length ? 
-        <div>
-            <button className={styles.skip}>Skip</button>
-            <img src={memes[0].url}/>
+        <div className={styles.container}>
+            <button onClick={() => console.log('g')} className={styles.generate}>Generate</button>
+            <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip}>Skip</button>
+            <img src={memes[memeIndex].url}/>
         </div> : 
         <></>
     )
