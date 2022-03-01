ac1dee1ecb2b4e496f012a407d664157aaa3cf28
jcsgh <business.joshuac@gmail.com>
2022-03-01 16:09:38 -0600

    get meme to work

--- src/MemeGenerated/MemeGenerated.js
+++ src/MemeGenerated/MemeGenerated.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import styles from './styles.module.css'
 
 export const MemeGenerated = () => {
-    return(<>Hello</>)
+
+    const history = useHistory();
+    const location = useLocation();
+    const url = new URLSearchParams(location.search).get('url');
+
+
+    return (
+        <div className={styles.container}>
+            <button onClick={() => history.push('/')} className={styles.home}>
+                Make More Memes
+            </button>
+            {url && <img src={url}/>}
+        </div>
+    )
 }
 
