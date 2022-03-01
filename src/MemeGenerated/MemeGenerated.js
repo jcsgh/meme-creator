250cd07adeb06c3739ed9567b8a36b3b594cd18e
jcsgh <business.joshuac@gmail.com>
2022-03-01 16:21:48 -0600

    app working

--- src/MemeGenerated/MemeGenerated.js
+++ src/MemeGenerated/MemeGenerated.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './styles.module.css';
 import { useHistory, useLocation } from 'react-router-dom';
-import styles from './styles.module.css'
+import { useClipboard } from 'use-clipboard-copy';
 
 export const MemeGenerated = () => {
 
-    const history = useHistory();
-    const location = useLocation();
-    const url = new URLSearchParams(location.search).get('url');
+  const [copied, setCopied] = useState(false);
 
+  const clipboard = useClipboard();
+  const history = useHistory();
+  const location = useLocation();
+  const url = new URLSearchParams(location.search).get('url');
 
-    return (
-        <div className={styles.container}>
-            <button onClick={() => history.push('/')} className={styles.home}>
-                Make More Memes
-            </button>
-            {url && <img src={url}/>}
-        </div>
-    )
-}
+  const copyLink = () => {
+    clipboard.copy(url);
+    setCopied(true);
+  };
 
+  return(
+    <div className={styles.container}>
+      <button onClick={() => history.push('/')} className={styles.home}>
+        Make More Memes
+      </button>
+      { url && <img alt='meme' src={url} /> }
+      <button onClick={copyLink} className={styles.copy}>
+        {copied ? 'Link copied!' : 'Copy link'}
+      </button>
+    </div>
+  );
+};
